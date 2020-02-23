8202c7f0704d59c52955cc0a7ce577e6d629e23a
Stephanie Johnson <Stephanie.Johnson@ucsf.edu>
2020-02-22 20:28:50 -0800

    some thoughts on saving

--- scripts/analyze_experiment.py
+++ scripts/analyze_experiment.py
@@ -70,6 +70,50 @@ if __name__ == '__main__':
       raise TypeError(trace_data)
 
   samples = analyze.run_mcmc(config, traces)
+  
+  # TODO save samples
 
-  # TODO save results
+  # Samples is a list of SamplerStates, one for each iteration of the MCMC,
+  # with the first element the initialization.
+  # SamplerState is a named tuple composed of a named tuple, SlopeyDurationDistn,
+  # which contains the a and b parameters that parameterize the gamma on slopeys, and
+  # a dict of trace_name : TraceLatentVars named tuple key-value pairs.
+  # TraceLatentVars contains a list ['times', 'red_vals', 'time_offset', 
+  # 'green_channel_transform_slope', 'green_channel_transform_intercept', 'camera_noise_sigma']
+  
+  # My old matlab plotting code expects the results of saving a dict via savemat from
+  # scipy.io. The dict was key-value pairs of tracenames and a dict
+  #           {'params': params, 'data': data,
+  #           'times_samples': times_samples, 'vals_samples': vals_samples,
+  #           'u_samples': u_samples, 'ch2_samples': ch2_samples,
+  #           'sigma_samples': sigma_samples} 
+  # When loaded into matlab, the shapes of these dict values were:
+  # times_samples: num_samples+1 by num_slopey+1 array
+  # vals_samples: num_samples+1 by num_slopey array
+  # ch2_samples: num_samples by 2 array
+  # u_samples: num_samples by 1 vector of offsets
+  # And data was num_frames by 2
+  
+  # Plan the first: re-create something similar so I can use my old matlab plotting code
+  # without too much rewriting. The main difference will be each trace file will now 
+  # also contain the a, b parameters for the slopey duration distribution (which will be the
+  # same for all trace files in a directory, because they were analyzed together).
+  
+  # Replace with list comprehensions or equivalent after I get the form down:
+  # I think I also need to import our named tuples or define them at the top?
+  
+  # make empty arrays for times_samples etc same shapes as in my original matlab code
+  # including a list of trace_names
+  
+  for s in samples:
+    # Iterate through samples and pull out the values for this iteration, dump
+    # them in arrays.
+    
+    # do I also want to save the data like I originally did?
+  
+  all_results = dict(t: v for t, v in zip(trace_names, trace_vals)
+  
+  
+  
+  scipy.io.savemat(outfile, all_results, long_field_names=True, oned_as='column')
 
